3311a3944f294975a653a95a6b5ead65fd8d0dd4
Siyuan Jiang <2203934+sjiang1@users.noreply.github.com>
2024-04-04 13:27:02 +0000

    Add a test for fastapi

--- backend/test_item.py
+++ backend/test_item.py
@@ -4,4 +4,13 @@ def test_add_an_item():
     add_an_item(my_item)
     assert(len(items) > 0)
     assert(items[-1].name == "test_item")
-    assert(items[-1].price == 1)
+
+
+from fastapi.testclient import TestClient
+from main import app
+client = TestClient(app)
+
+def test_add_an_item_API():
+    response = client.get("/items/123")
+    assert(response.status_code == 200)
+    assert(response.json() == {"item_id": 123, "q": "hello"})
